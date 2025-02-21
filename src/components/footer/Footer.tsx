ba12678a47a5ee026c35aca4fb2442a4e6399aeb
Camilo Pinzon <pinzonac@gmail.com>
2025-02-20 22:28:01 -0500

    Partial footer

--- src/components/footer/Footer.tsx
+++ src/components/footer/Footer.tsx
@@ -1,9 +1,36 @@
+import { Link } from "react-router-dom";
+
+import Image from "../image/Image";
+
+import linkedIn from "../../assets/linkedIn.svg";
+import gitHub from "../../assets/github.svg";
+import leetCode from "../../assets/leetcode.svg";
+
 import "./footer.scss";
 
 const Footer = () => {
 	return (
 		<div className="footer">
-			<h1 className="text-3xl font-bold underline">Footer</h1>
+			<div className="footer__up">
+				<div className="footer__up-left">
+					<h3>CAMILO PINZÓN</h3>
+					<i>Web Developer</i>
+					<div className="footer__up-left--social">
+						<Link
+							to={`https://www.linkedin.com/in/camilo-pinzon/`}
+							target="_blank"
+						>
+							<Image src={linkedIn} alt="LinkedIn" kind="icon" />
+						</Link>
+					</div>
+				</div>
+				<div className="footer__up-center">
+					<div>Footer menú</div>
+				</div>
+			</div>
+			<div className="footer__down">
+				<div>Legal Menú</div>
+			</div>
 		</div>
 	);
 };
