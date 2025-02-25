a593062ba3b58ef30017d9373d92ec63394bcab2
Camilo Pinzon <pinzonac@gmail.com>
2025-02-25 04:42:42 -0500

    Contact hero

--- src/pages/Contact/Contact.tsx
+++ src/pages/Contact/Contact.tsx
@@ -1,7 +1,20 @@
+import "./contact.scss";
+
 const Contact = () => {
 	return (
-		<div>
-			<h1>Contact Page</h1>
+		<div className="contact">
+			<div className="contact__hero">
+				<div className="contact__hero-container container">
+					<h1>Let's Collaborate.</h1>
+					<p>
+						<i>
+							Feel free to reach out anytime! Whether you need assistance or
+							just want to connect, send me a message, and let's build something
+							great together.
+						</i>
+					</p>
+				</div>
+			</div>
 		</div>
 	);
 };
