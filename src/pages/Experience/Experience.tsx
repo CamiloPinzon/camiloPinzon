9cb61c31a4658a6aa321755bc9cc3e1dec1f7ae8
Camilo Pinzon <pinzonac@gmail.com>
2025-03-05 00:16:06 -0500

    Experience data, hero

--- src/pages/Experience/Experience.tsx
+++ src/pages/Experience/Experience.tsx
@@ -1,8 +1,70 @@
+import { Link } from "react-router-dom";
+import Hero from "../../components/hero/Hero";
+import { experienceContent } from "../../data/experience.json";
+import "./experience.scss";
+
+interface ExperienceDataI {
+	id: number;
+	company: string;
+	position: string;
+	dateStart: string;
+	dateEnd: string;
+	description: string;
+	technologies: string[];
+	link?: string;
+}
+
 const Experience = () => {
+	const paragraph =
+		"Experienced web developer crafting high-quality, scalable, and efficient digital solutions.";
 	return (
-		<div>
-			<h1>Experience Page</h1>
-		</div>
+		<section className="experience-container">
+			<div className="experience-hero">
+				<Hero
+					bgImage="./images/experienceHero.jpg"
+					title="Bringing Ideas to Life with Code"
+					paragraph={paragraph}
+					style="light"
+				/>
+			</div>
+			<main className="experience-list container">
+				{experienceContent.map((item: ExperienceDataI) => {
+					const ExperienceContent = () => (
+						<div className="experience-item-content">
+							<div className="experience-item-content__left">
+								<p>
+									{item.dateStart} - {item.dateEnd}
+								</p>
+							</div>
+							<div className="experience-item-content__right">
+								<h2>{item.company}</h2>
+								<p>{item.position}</p>
+								<p>{item.description}</p>
+								<div className="technologies">
+									{item.technologies.map((tech, index) => (
+										<span key={index} className="tech-tag">
+											{tech}
+										</span>
+									))}
+								</div>
+							</div>
+						</div>
+					);
+
+					return (
+						<article key={item.id} className="experience-item">
+							{item.link ? (
+								<Link to={item.link}>
+									<ExperienceContent />
+								</Link>
+							) : (
+								<ExperienceContent />
+							)}
+						</article>
+					);
+				})}
+			</main>
+		</section>
 	);
 };
 
