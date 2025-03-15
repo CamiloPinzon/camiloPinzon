1e461de1f7d014fc5e6f4d11a868d9ac1a3ff11b
Camilo Pinzon <pinzonac@gmail.com>
2025-03-15 00:56:41 -0500

    blog page share component fb function

--- src/components/sharePost/SharePost.tsx
+++ src/components/sharePost/SharePost.tsx
@@ -11,7 +11,7 @@ const SharePost: React.FC<SharePostProps> = ({ title, url }) => {
 	const encodedUrl = encodeURIComponent(url);
 
 	const shareLinks = {
-		facebook: `https://www.facebook.com/sharer/sharer.php?u=${encodedUrl}`,
+		facebook: `https://www.facebook.com/sharer/sharer.php?kid_directed_site=0&u=${encodedUrl}&display=popup&ref=plugin&src=share_button`,
 		twitter: `https://twitter.com/intent/tweet?text=${encodedTitle}&url=${encodedUrl}`,
 		linkedin: `https://www.linkedin.com/sharing/share-offsite/?url=${encodedUrl}`,
 		whatsapp: `https://wa.me/?text=${encodedTitle}%20${encodedUrl}`,
