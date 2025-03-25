b4648695b5d1593fb28b1d30e4ec9cf8cd8cb929
Camilo Pinzon <pinzonac@gmail.com>
2025-03-25 11:25:04 -0500

    Added instagram and X links in social icons

--- src/components/socialIcons/socialIcons.tsx
+++ src/components/socialIcons/socialIcons.tsx
@@ -4,6 +4,8 @@ import Image from "../image/Image";
 import linkedIn from "../../assets/linkedIn.svg";
 import gitHub from "../../assets/github.svg";
 import leetCode from "../../assets/leetcode.svg";
+import instagram from "../../assets/instagram.svg";
+import x from "../../assets/x.svg";
 
 import "./socialIcons.scss";
 
@@ -11,6 +13,8 @@ const SocialIcons = () => {
 	const LinkedInUrl = "https://www.linkedin.com/in/camilo-pinzon/";
 	const GitHubUrl = "https://github.com/CamiloPinzon";
 	const LeetCodeUrl = "https://leetcode.com/u/pinzonac/";
+	const InstagramURL = "https://www.instagram.com/camilopinzon_developer";
+	const XUrl = "https://x.com/CamiloPinzonDev";
 
 	return (
 		<div className="social-icons">
@@ -22,6 +26,12 @@ const SocialIcons = () => {
 			</Link>
 			<Link to={LeetCodeUrl} target="_blank">
 				<Image src={leetCode} alt="LeetCode" kind="icon" />
+			</Link>
+			<Link to={InstagramURL} target="_blank">
+				<Image src={instagram} alt="Instagram" kind="icon" />
+			</Link>
+			<Link to={XUrl} target="_blank">
+				<Image src={x} alt="X" kind="icon" />
 			</Link>
 		</div>
 	);
