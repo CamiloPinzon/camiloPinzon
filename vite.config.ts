267bc515f2fea0ef57f63bebdfee290a5420e998
Camilo Pinzon <pinzonac@gmail.com>
2025-03-26 00:13:00 -0500

    Change compression config

--- vite.config.ts
+++ vite.config.ts
@@ -3,13 +3,24 @@ import react from "@vitejs/plugin-react";
 import compression from "vite-plugin-compression";
 
 export default defineConfig({
+	build: {
+		rollupOptions: {
+			output: {
+				manualChunks(id) {
+					if (id.includes("node_modules")) {
+						return "vendor";
+					}
+				},
+			},
+		},
+	},
 	plugins: [
 		react(),
 		compression({
 			verbose: true,
 			algorithm: "gzip",
 			ext: ".gz",
-			threshold: 10240,
+			threshold: 1024,
 			deleteOriginFile: false,
 		}),
 	],
