e2a47a6a15cf2601cfc6015bc91e1c0400d81767
Camilo Pinzon <pinzonac@gmail.com>
2025-04-03 23:42:38 -0500

    Confirm before send

--- src/components/admin/adminMenu/AdminMenu.tsx
+++ src/components/admin/adminMenu/AdminMenu.tsx
@@ -3,6 +3,33 @@ import { NavLink } from "react-router-dom";
 import "../../mainMenu/mainMenu.scss";
 
 const AdminMenu = () => {
+	const handleSendNewsletter = () => {
+		//ask for confirmation before sending the newsletter
+		if (!window.confirm("Are you sure you want to send the newsletter?")) {
+			return;
+		}
+		//send the newsletter to the server
+		const url =
+			"http://camilopinzon.netlify.app/.netlify/functions/send-newsletter";
+		window
+			.fetch(url, {
+				method: "POST",
+				headers: {
+					"Content-Type": "application/json",
+				},
+			})
+			.then((response) => {
+				if (response.ok) {
+					alert("Newsletter sent successfully!");
+				} else {
+					alert("Error sending newsletter. Please try again later.");
+				}
+			})
+			.catch((error) => {
+				alert("Error sending newsletter. Please try again later.");
+				console.error("Error sending newsletter: ", error);
+			});
+	};
 	return (
 		<nav className="main-menu">
 			<NavLink
@@ -37,12 +64,7 @@ const AdminMenu = () => {
 			>
 				Create New Blog
 			</NavLink>
-			<a
-				href="http://camilopinzon.netlify.app/.netlify/functions/send-newsletter"
-				target="_popup"
-				className="main-menu__link"
-				rel="noopener noreferrer"
-			>
+			<a href="#" className="main-menu__link" onClick={handleSendNewsletter}>
 				Send newsletter email
 			</a>
 		</nav>
